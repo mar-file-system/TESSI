1d0b6bd3bb9635b01f85934e0733a9b5feb8a5d0
John Bent <jbent@newmexicoconsortium.org>
2024-01-10 17:24:42 -0700

    Got an ansible playbook almost working for zfs build

--- scripts/revert_VM.sh
+++ scripts/revert_VM.sh
@@ -5,6 +5,7 @@ vm_name="tassi01"
 snap="centos8streamsnap"
 snap="passwordless_ssh"
 snap="git_installed"
+snap="ansible_ready"
 
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
