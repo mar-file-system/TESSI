6f2834b71abf0a139146d80d485bffc41e566918
John Bent <jbent@newmexicoconsortium.org>
2024-01-12 13:53:03 -0700

    Added missing git dependency

--- scripts/build_zfs.sh
+++ scripts/build_zfs.sh
@@ -9,6 +9,9 @@ fi
 dnf install -y --skip-broken epel-release gcc make autoconf automake libtool rpm-build kernel-rpm-macros libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel zlib-devel libaio-devel libattr-devel elfutils-libelf-devel kernel-devel-$(uname -r) kernel-abi-stablelists-$(uname -r | sed 's/\.[^.]\+$//') python3 python3-devel python3-setuptools python3-cffi libffi-devel ncompress
 dnf install -y --skip-broken --enablerepo=epel --enablerepo=powertools python3-packaging dkms
 
+# make sure git is installed
+dnf install -y git
+
 # clone and configure and build zfs
 git clone https://github.com/openzfs/zfs.git
 cd zfs/
