764e9151505ce7a4b85c083446fd5e60ef7d2e72
John Bent <jbent@newmexicoconsortium.org>
2024-01-19 16:39:30 -0700

    maybe working? will test on in15 while I wait on in16

--- scripts/setup_VM3.sh
+++ scripts/setup_VM3.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-USAGE="$0 hostname [must be run as root]"
+USAGE="USAGE: $0 hostname [must be run as root]"
 if [ "$(id -u)" != "0" ] || [ $# -eq 0 ]; then
     echo $USAGE
     exit 1
@@ -15,23 +15,10 @@ disk_size=12
 centos8="http://mirror.centos.org/centos/8-stream"
 baseos_location="$centos8/BaseOS/x86_64/os/"
 appstr_location="$centos8/AppStream/x86_64/os/"
-ip=`hostname -I | awk '{print $1}'`
+web_server_ip=`hostname -I | awk '{print $1}'`
 root_password="password"
 
-# Function to find an available port
-find_available_port() {
-    for port in $(seq 8000 8100); do
-        echo > /dev/tcp/localhost/$port
-        if [ $? -ne 0 ]; then
-            echo $port
-            return
-        fi
-    done
-    echo "No available port found" >&2
-    exit 1
-}
-
-# Prepare kickstart file
+# Prepare kickstart file and chdir to the directory where we create it
 ks_dir="kickstart_files"
 mkdir -p $ks_dir
 cd $ks_dir
@@ -65,12 +52,40 @@ pwpolicy luks --minlen=6 --minquality=1 --notstrict --nochanges --notempty
 %end
 EOF
 
-# Start a web server on an available port
-available_port=$(find_available_port)
-python3 -m http.server $available_port &
+# Function to start web server on first available port
+start_web_server() {
+    local port=8000
 
-# Store web server PID to kill it later
+    while true; do
+        # Try to start the server in the background
+        python3 -m http.server $port --bind $web_server_ip &>/dev/null &
+        local pid=$!
+
+        # Wait a moment to see if the server starts successfull
+        sleep 2
+
+        # Check if the process is still running
+        if ps -p $pid > /dev/null; then
+            echo $port
+            return
+        else
+            # If not running, assume it's because it couldn't find a port so try the next port
+            ((port++))
+        fi
+
+        # Check if port range exceeded
+        if [ $port -gt 8100 ]; then
+            echo "No available port found" >&2
+            exit 1
+        fi
+    done
+}
+
+# Start web server and get the allocated port and the child PID
+web_server_port=$(start_web_server)
 web_server_pid=$!
+
+echo "Web server is now running on $web_server_ip:$web_server_port"
 
 virt-install \
 --name "$hostname" \
@@ -83,7 +98,9 @@ virt-install \
 --graphics none \
 --console pty,target_type=serial \
 --location "$baseos_location" \
---extra-args 'console=ttyS0 ks=http://$ip:$available_port/$hostname.kickstart'
+--extra-args "console=ttyS0 ks=http://$web_server_ip:$web_server_port/$hostname.kickstart" \
+--noautoconsole \
+--wait -1
 
 # Kill the web server
 kill $web_server_pid
