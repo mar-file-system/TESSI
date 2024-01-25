54d60237d0daca8d7d3d58b5c70e15048fa7f660
John Bent <jbent@newmexicoconsortium.org>
2024-01-25 09:05:58 -0700

    few small changes

--- scripts/test_lustre.sh
+++ scripts/test_lustre.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+testfile=/mnt/lustre/file.out
+
 # Get the primary IP address
 IP_ADDRESS=$(hostname -I)
 
 # Set the hostname 
-hname=mylustrehost
-hostnamectl set-hostname $hname 
+# not necessary anymore since the VM creation tools handle this
+# hname=mylustrehost
+# hostnamectl set-hostname $hname 
 
 # also update /etc/hosts with this new hostname
-echo "$IP_ADDRESS $hname" >> /etc/hosts
+# hmmm, I wonder if this is necessary?
+echo "No longer setting the hostname in /etc/hosts, is that a problem?"
+# echo "$IP_ADDRESS $hname" >> /etc/hosts
 
 # first clean it up in case we forgot to do that
 ./lustre/tests/llmountcleanup.sh
@@ -18,8 +23,15 @@ FSTYPE=zfs ./lustre/tests/llmount.sh
 
 df -h /mnt/lustre
 ./lustre/utils/lfs getstripe /mnt/lustre/
+dd if=/dev/zero of=$testfile bs=1MB count=400
+df -h /mnt/lustre-*
+\rm $testfile
+
+# change striping behavior, 
+# write a large file and then check it was striped across all osts
 ./lustre/utils/lfs setstripe -c -1 /mnt/lustre
 ./lustre/utils/lfs getstripe /mnt/lustre/
 
-dd if=/dev/zero of=/mnt/lustre/file.out bs=1MB count=400
-
+dd if=/dev/zero of=$testfile bs=1MB count=400
+df -h /mnt/lustre-*
+\rm $testfile
