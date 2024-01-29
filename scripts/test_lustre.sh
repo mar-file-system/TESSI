b90e5565238c2a867750829c9bb0e077376748cb
John Bent <jbent@newmexicoconsortium.org>
2024-01-29 12:46:42 -0700

    Added a function to simplify the script

--- scripts/test_lustre.sh
+++ scripts/test_lustre.sh
@@ -4,10 +4,18 @@ cd /usr/local/src/lustre-release
 
 LLMOUNT="./lustre/tests/llmount.sh"
 LLMOUNT_CLEANUP="./lustre/tests/llmountcleanup.sh"
+
 [ ! -f "$LLMOUNT" ] && { echo "Warning: $LLMOUNT does not exist."; exit 1; }
 [ "$(id -u)" -ne 0 ] && { echo "Warning: This script must be run as root."; exit 1; }
 
-testfile=/mnt/lustre/file.out
+test_striping() {
+    FILESIZE_MB=200
+    testfile=/mnt/lustre/file.out
+    dd if=/dev/zero of=$testfile bs=1MB count=$FILESIZE_MB
+    sleep 3
+    stripes=`df -h /mnt/lustre-ost* | grep -v Filesystem | awk '{print $3}' | tr '\n' ' '`
+    echo "Stripes with $1 pattern: $stripes"
+}
 
 # first clean it up in case we forgot to do that
 $LLMOUNT_CLEANUP
@@ -17,17 +25,10 @@ FSTYPE=zfs $LLMOUNT
 # test the default striping behavior
 df -h /mnt/lustre
 ./lustre/utils/lfs getstripe /mnt/lustre/
-dd if=/dev/zero of=$testfile bs=1MB count=400
-echo "The default striping pattern"
-df -h /mnt/lustre-*
-\rm $testfile
+test_striping "default"
 
 # change striping behavior, 
-# write a large file and then check it was striped across all osts
 ./lustre/utils/lfs setstripe -c -1 /mnt/lustre
 ./lustre/utils/lfs getstripe /mnt/lustre/
 
-dd if=/dev/zero of=$testfile bs=1MB count=400
-echo "The modified striping pattern"
-df -h /mnt/lustre-*
-\rm $testfile
+test_striping "modified"
