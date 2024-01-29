99256bd7e93f19bb141b36cc09016b9e06a6cb32
John Bent <jbent@newmexicoconsortium.org>
2024-01-29 11:27:08 -0700

    fixed typo

--- scripts/test_lustre.sh
+++ scripts/test_lustre.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /usr/local/src/lustre_release
+cd /usr/local/src/lustre-release
 
 LLMOUNT="./lustre/tests/llmount.sh"
-[ ! -f "$LLMOUNT_SH" ] && { echo "Warning: $LLMOUNT_SH does not exist."; exit 1; }
+[ ! -f "$LLMOUNT" ] && { echo "Warning: $LLMOUNT does not exist."; exit 1; }
 [ "$(id -u)" -ne 0 ] && { echo "Warning: This script must be run as root."; exit 1; }
 
 testfile=/mnt/lustre/file.out
