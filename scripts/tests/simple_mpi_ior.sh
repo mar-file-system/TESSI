5633bfa3008ed9a7a198d49b133c1e443c5ae766
John Bent <jbent@newmexicoconsortium.org>
2024-04-22 12:46:18 -0600

    Added simple test script

---
+++ scripts/tests/simple_mpi_ior.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Environment variables for MPI
+mpi_env_vars="export OMPI_MCA_pml=ob1; export OMPI_MCA_btl=tcp,self;"
+
+# Arguments for IOR command
+ior_args="--allow-run-as-root -np 2 ior -o /mnt/lustre/testfile -host client01,client00 -t 1m -b 16m -s 16"
+
+# Execute the command on the remote host
+ssh client00 "${mpi_env_vars} mpirun ${ior_args}"
