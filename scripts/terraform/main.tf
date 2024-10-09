a8c07aa429c29109ef39d3f5143b40297a1bdad1
John Bent <jbent@newmexicoconsortium.org>
2024-10-09 00:29:29 -0600

    got something working to create VMs with terraform

--- scripts/terraform/main.tf
+++ scripts/terraform/main.tf
@@ -1,3 +1,10 @@
+# from https://www.youtube.com/watch?v=Lt8cHwy-jEM
+# from https://low-orbit.net/terraform-kvm
+# note the comment on the youtube which explains that the qcow2 image was previously created
+# I modified to use the gold image which I created by:
+# sudo virsh domblklist gold-beegfs-almalinux8-server # to find the path
+# qemu-img convert -O qcow2 <path> <new_path.qcow2>
+
 terraform {
   required_providers {
     libvirt = {
@@ -5,79 +12,47 @@ terraform {
     }
   }
 }
-
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# Create a new storage pool in a unique directory
-resource "libvirt_pool" "terraform_storage_pool" {
-  name = "terraform_new_pool"
-  type = "dir"
-  path = "/var/lib/libvirt/terraform_images"  # Specify a new path for the pool
+locals {
+    host_list = toset([ "host1", "host2", "host3" ])
 }
 
-# Create a volume in the new storage pool
-resource "libvirt_volume" "terraform_boot_hdd" {
-  name   = "terraform_boot_hdd.qcow2"
-  pool   = libvirt_pool.terraform_storage_pool.name
-  size   = 12         # Size in GB
-  format = "qcow2"
+resource "libvirt_volume" "volumes" {
+    for_each = local.host_list
+        name = "${each.key}.qcow2"
+        pool = "default"
+        source = "/var/lib/libvirt/images/gold-beegfs-almalinux8-server.qcow2"
+        format = "qcow2"
 }
 
-resource "libvirt_cloudinit_disk" "commoninit" {
-  name      = "commoninit.iso"
-  pool      = libvirt_pool.terraform_storage_pool.name
-  user_data = <<-EOT
-    #cloud-config
-    password: password
-    chpasswd: { expire: False }
-    ssh_authorized_keys:
-      - $(cat /home/jbent/.ssh/authorized_keys)
-  EOT
-}
+resource "libvirt_domain" "hosts" {
+    for_each = local.host_list
+        name   = each.key
+        memory = "2048"
+        vcpu   = 2
 
-resource "libvirt_domain" "bootstrap_vm" {
-  name   = "bootstrap_vm"
-  memory = 4096       # Memory in MB
-  vcpu   = 2
+        network_interface {
+            network_name = "hostonly-net"
+        }
 
-  # Attach the boot volume (HDD)
-  disk {
-    volume_id = libvirt_volume.terraform_boot_hdd.id
-  }
+        disk {
+            volume_id = libvirt_volume.volumes[each.key].id
+       }
 
-  # Attach the CD-ROM with the installation media
-  disk {
-    file = "/mnt/usrc-storage-nfs/jbent/images/alma/AlmaLinux-8.10-x86_64-minimal.iso"
-  }
+        console {
+            type = "pty"
+            target_type = "serial"
+            target_port = "0"
+        }
 
-  # Attach the Cloud-init ISO by explicitly providing the file path
-  disk {
-    file = "/var/lib/libvirt/terraform_images/commoninit.iso"
-  }
-
-  # Network interface
-  network_interface {
-    network_name = "default"
-  }
-
-  # Console setup (you need to specify a target port)
-  console {
-    type        = "pty"
-    target_type = "serial"
-    target_port = "0"
-  }
-
-  # Graphics (optional)
-  graphics {
-    type = "vnc"
-    listen_type = "none"
-  }
-
-  # Boot configuration
-  boot_device {
-    dev = ["cdrom", "hd"]
-  }
+        graphics {
+            type = "vnc"
+            listen_type = "address"
+            listen_address = "0.0.0.0"
+            autoport = true
+        }
 }
 
