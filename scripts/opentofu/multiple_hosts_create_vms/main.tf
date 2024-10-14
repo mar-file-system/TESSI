d11358f6b9e43392451b18b52aa8c7bb42940ff3
John Bent <jbent@newmexicoconsortium.org>
2024-10-13 22:56:54 -0600

    Now works across in07 and in16 with version 0.8.0 after setting some stuff in ~/.ssh/config

--- scripts/opentofu/multiple_hosts_create_vms/main.tf
+++ scripts/opentofu/multiple_hosts_create_vms/main.tf
@@ -3,7 +3,10 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
-      version = "0.6.14"
+      #version = "0.6.14" # this works
+      #version = "0.8.0"  # this crashes
+      #version = "0.7.6"   # this works
+      # refer to ../minimal_broken/README for instructions to fix with 0.8.0
     }
   }
 }
@@ -13,42 +16,54 @@ provider "libvirt" {
 }
 
 provider "libvirt" {
-  alias = "remotehost"
+  alias = "in07"
   uri   = "qemu+ssh://root@in07/system"
 }
 
-resource "libvirt_volume" "local-qcow2" {
-  name   = "local-qcow2"
+provider "libvirt" {
+  alias = "in16"
+  uri   = "qemu+ssh://root@in16/system"
+}
+
+resource "libvirt_volume" "in07-qcow2" {
+  provider = libvirt.in07
+  name   = "in07-qcow2"
   pool   = "default"
   format = "qcow2"
   size   = 100000
 }
 
-resource "libvirt_volume" "remotehost-qcow2" {
-  provider = libvirt.remotehost
-  name     = "remotehost-qcow2"
+resource "libvirt_volume" "in16-qcow2" {
+  provider = libvirt.in16
+  name     = "in16-qcow2"
   pool     = "default"
   format   = "qcow2"
   size     = 100000
 }
 
-resource "libvirt_domain" "local-domain" {
-  name   = "local"
-  memory = "2048"
-  vcpu   = 2
-
-  disk {
-    volume_id = libvirt_volume.local-qcow2.id
-  }
-}
-
-resource "libvirt_domain" "remotehost-domain" {
-  provider = libvirt.remotehost
-  name     = "remotehost"
+resource "libvirt_domain" "in07-domain" {
+  provider = libvirt.in07
+  name     = "in07vm01"
   memory   = "2048"
   vcpu     = 2
 
   disk {
-    volume_id = libvirt_volume.remotehost-qcow2.id
+    volume_id = libvirt_volume.in07-qcow2.id
   }
+
+  depends_on = [libvirt_volume.in07-qcow2]
 }
+
+resource "libvirt_domain" "in16-domain" {
+  provider = libvirt.in16
+  name     = "in16vm01"
+  memory   = "2048"
+  vcpu     = 2
+
+  disk {
+    volume_id = libvirt_volume.in16-qcow2.id
+  }
+
+  depends_on = [libvirt_volume.in16-qcow2]
+}
+
