0b9956dbb6632bebe84972afc1730d66ada7a712
John Bent <jbent@newmexicoconsortium.org>
2024-10-14 01:07:20 -0600

    Working parallel creation of VMs

---
+++ scripts/opentofu/beegfs_2c2m2s/vm_creation.tf
@@ -0,0 +1,85 @@
+resource "libvirt_domain" "in07_vms" {
+  provider = libvirt.in07
+  for_each   = { for vm in var.vm_name : vm.name => vm if vm.host == "in07"}
+  name       = "${each.value.name}"
+  memory     = each.value.memory  # Default memory if not specified
+  vcpu       = each.value.vcpu    # Default vCPU if not specified
+  running    = true
+  autostart  = true
+  machine    = each.value.machine
+
+  # Attach primary disk
+  disk {
+    volume_id = libvirt_volume.in07_volumes[each.key].id
+  }
+
+  # Attach secondary disk if it exists
+  dynamic "disk" {
+    for_each = (each.value.second_disk_size != null) ? [1] : []
+    content {
+      volume_id = libvirt_volume.in07_second_volumes[each.key].id
+    }
+  }
+
+  network_interface {
+    network_name = "${each.value.network}"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  # Ensure each domain waits for all volumes
+  # it'd be more parallel to have each VM wait on only its own volumes but that is hard since some have two volumes and some have just one
+  #   and depends_on only works on static lists so we can't create conditional dependencies
+  depends_on = [
+    libvirt_volume.in07_volumes,
+    libvirt_volume.in07_second_volumes
+  ]
+
+}
+
+resource "libvirt_domain" "in16_vms" {
+  provider = libvirt.in16
+  for_each   = { for vm in var.vm_name : vm.name => vm if vm.host == "in16"}
+  name       = "${each.value.name}"
+  memory     = each.value.memory  # Default memory if not specified
+  vcpu       = each.value.vcpu    # Default vCPU if not specified
+  running    = true
+  autostart  = true
+  machine    = each.value.machine
+
+  # Attach primary disk
+  disk {
+    volume_id = libvirt_volume.in16_volumes[each.key].id
+  }
+
+  # Attach secondary disk if it exists
+  dynamic "disk" {
+    for_each = (each.value.second_disk_size != null) ? [1] : []
+    content {
+      volume_id = libvirt_volume.in16_second_volumes[each.key].id
+    }
+  }
+
+  network_interface {
+    network_name = "${each.value.network}"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  # Ensure each domain waits for all volumes
+  # it'd be more parallel to have each VM wait on only its own volumes but that is hard since some have two volumes and some have just one
+  #   and depends_on only works on static lists so we can't create conditional dependencies
+  depends_on = [
+    libvirt_volume.in16_volumes,
+    libvirt_volume.in16_second_volumes
+  ]
+
+}
---
+++ scripts/opentofu/beegfs_2c2m2s/vm_declarations.tf
@@ -0,0 +1,45 @@
+
+variable "vm_name" {
+  type = list(object({
+    name             = string
+    machine          = optional(string, "pc-q35-rhel8.2.0")
+    memory           = optional(string, "2048")
+    vcpu             = optional(number, 2)
+    second_disk_size = optional(number, null)
+    host             = string  # The short-hand for the host (e.g., "in07" or "in16")
+    source_image     = optional(string, "/var/lib/libvirt/images/gold-beegfs-almalinux8-server.qcow2")  # Optional source image with default
+    storage_pool     = optional(string, "default")
+    network          = optional(string, "hostonly-net")
+  }))
+  default = [
+    {
+      name            = "beegfs-client00"
+      host            = "in07"
+    },
+    {
+      name            = "beegfs-client01"
+      host            = "in07"
+    },
+    {
+      name            = "beegfs-meta00"
+      second_disk_size = 5
+      host            = "in07"
+    },
+    {
+      name            = "beegfs-meta01"
+      second_disk_size = 5
+      host            = "in16"
+    },
+    {
+      name            = "beegfs-data00"
+      second_disk_size = 10
+      host            = "in16"
+    },
+    {
+      name            = "beegfs-data01"
+      second_disk_size = 10
+      host            = "in16"
+    }
+  ]
+}
+
