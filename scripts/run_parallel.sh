8ff642de2fb94361d3c033fe91cbeb7bec23301b
John Bent <jbent@newmexicoconsortium.org>
2025-01-09 21:49:22 -0700

    The workflow to run the new parallel TASSI is getting confusing so I've encoded it in this file temporarily. Eventually will replace with a fancier driver

---
+++ scripts/run_parallel.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+CREATE_WG=wireguard
+CREATE_VMS=opentofu/dynamic_hosts_libvirt_vms/
+CREATE_GRAPH=network_graph
+DEBUG_NETWORK=network_graph
+
+phase() {
+  local message="$1"
+  local length=${#message}
+  local border=$(printf '#%.0s' $(seq 1 $((length + 10))))
+  
+  echo
+  echo "$border"
+  echo "#####  $message  #####"
+  echo "$border"
+  echo
+}
+
+phase "Starting Parallel Run of TASSI"
+
+phase "Cleaning up VMs"
+pushd .
+cd $CREATE_VMS
+sudo tofu destroy --auto-approve
+popd
+
+phase "Cleaning up Networking"
+pushd .
+cd $CREATE_WG
+sudo ansible-playbook -i inventory.yaml create_wireguard_tunnels.yaml --tags cleanup
+popd
+
+phase "Create Networking and Tofu System Description"
+pushd .
+cd $CREATE_WG
+sudo ansible-playbook -i inventory.yaml create_wireguard_tunnels.yaml 
+popd
+
+phase "Create VMs Using the Auto-Generated Tofu System Description"
+pushd .
+cd $CREATE_VMS
+sudo cp /tmp/system_description.tf .
+sudo tofu apply --auto-approve
+popd
+
+phase "Create Graph"
+pushd .
+cd $CREATE_GRAPH
+sudo ansible-playbook -i inventory.yaml make_network_graph.yaml 
+sudo cp graph.png /tmp
+popd
+
+phase "Debug Network"
+pushd .
+cd $CREATE_GRAPH
+sudo ansible-playbook -i inventory.yaml debug_network.yaml 
+popd
