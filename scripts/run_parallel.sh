08247db836cf5fe4e034efc647d456b67eb5760e
John Bent <jbent@newmexicoconsortium.org>
2025-01-23 23:17:43 -0700

    Cleaned it up and added loops instead of just a long sequence of commands

--- scripts/run_parallel.sh
+++ scripts/run_parallel.sh
@@ -1,15 +1,35 @@
-#! /bin/bash -e
+#!/bin/bash -e
 
-CREATE_WG=wireguard
-CREATE_VMS=opentofu/dynamic_hosts_libvirt_vms/
-CREATE_GRAPH=network_graph
-DEBUG_NETWORK=network_graph
+# Define phases and their directories
+declare -A phases
+phases[gold_vms]="make_golds"
+phases[network]="wireguard"
+phases[vms]="opentofu/dynamic_hosts_libvirt_vms"
+phases[artifacts]="network_graph"
 
+# Define cleanup and create commands for each phase
+declare -A commands
+commands[gold_vms_clean]="ansible-playbook -i inventory.yaml make_golds.yaml --tags makeclean"
+commands[gold_vms_create]="ansible-playbook -i inventory.yaml make_golds.yaml"
+
+commands[network_clean]="ansible-playbook -i inventory.yaml create_wireguard_tunnels.yaml --tags makeclean"
+commands[network_create]="ansible-playbook -i inventory.yaml create_wireguard_tunnels.yaml"
+
+commands[vms_clean]="tofu destroy --auto-approve"
+commands[vms_create]="cp /tmp/system_description.tf . && tofu apply --auto-approve"
+
+commands[artifacts_clean]="\rm -f artifacts/*png"
+commands[artifacts_create]="ansible-playbook -i inventory.yaml make_network_graph.yaml && ansible-playbook -i inventory.yaml debug_network.yaml && ls -ltr artifacts/*png"
+
+# Define the ordered list of phases
+phase_order=("gold_vms" "network" "vms" "artifacts")
+
+# Define a function for phase messages
 phase() {
   local message="$1"
   local length=${#message}
   local border=$(printf '#%.0s' $(seq 1 $((length + 10))))
-  
+
   echo
   echo "$border"
   echo "#####  $message  #####"
@@ -17,67 +37,33 @@ phase() {
   echo
 }
 
-phase "Starting Parallel Run of TASSI"
+# Function to process a phase
+process_phase() {
+  local phase_name="$1"
+  local action="$2" # clean or create
+  local command_key="${phase_name}_${action}"
+  local dry_run=0 # Set to 1 for dry-run, 0 for execution
 
-phase "Cleaning up VMs"
-pushd .
-cd $CREATE_VMS
-sudo tofu destroy --auto-approve
-popd
+  if [[ -n "${commands[$command_key]}" ]]; then
+    pushd . > /dev/null
+    cd "${phases[$phase_name]}"
+    phase "${action^} $phase_name"
+    if [[ $dry_run -eq 1 ]]; then
+      echo "[Dry Run] cd ${phases[$phase_name]} && ${commands[$command_key]}"
+    else
+      sudo bash -c "${commands[$command_key]}"
+    fi
+    popd > /dev/null
+  fi
+}
 
-phase "Cleaning up Networking"
-pushd .
-cd $CREATE_WG
-sudo ansible-playbook -i inventory.yaml create_wireguard_tunnels.yaml --tags cleanup
-popd
-
-phase "Clean up Artifacts"
-pushd . 
-cd $CREATE_GRAPH
-sudo \rm -f artifacts/*png
-popd
-
-phase "Create Networking and Tofu System Description"
-pushd .
-cd $CREATE_WG
-sudo ansible-playbook -i inventory.yaml create_wireguard_tunnels.yaml 
-popd
-
-phase "Create VMs Using the Auto-Generated Tofu System Description"
-pushd .
-cd $CREATE_VMS
-sudo cp /tmp/system_description.tf .
-sudo tofu apply --auto-approve
-popd
-
-phase "Manually add Routes. TODO: Automate this somehow"
-# VMs on in07
-for vm in beegfs-meta00 beegfs-client01 beegfs-client00; do
-  #ssh in07 "ssh -o StrictHostKeyChecking=no root@$vm 'ip route add 192.68.2.0/24 via 192.68.3.1'"
-  echo "Not manually adding route to $vm. Hoping this is done automatically in kickstart"
+# Execute all cleanup phases in reverse order
+for (( idx=${#phase_order[@]}-1 ; idx>=0 ; idx-- )); do
+  process_phase "${phase_order[idx]}" "clean"
 done
 
-# VMs on in16
-for vm in beegfs-meta01 beegfs-data00 beegfs-data01; do
-  #ssh in16 "ssh -o StrictHostKeyChecking=no root@$vm 'ip route add 192.68.2.0/24 via 192.68.3.33'"
-  echo "Not manually adding route to $vm. Hoping this is done automatically in kickstart"
+# Execute all creation phases
+for phase_name in "${phase_order[@]}"; do
+  process_phase "$phase_name" "create"
 done
 
-
-phase "Create Network Graph"
-pushd .
-cd $CREATE_GRAPH
-sudo ansible-playbook -i inventory.yaml make_network_graph.yaml 
-popd
-
-phase "Debug Network"
-pushd .
-cd $CREATE_GRAPH
-sudo ansible-playbook -i inventory.yaml debug_network.yaml 
-popd
-
-phase "Look at Artifacts"
-pushd . 
-cd $CREATE_GRAPH
-ls -ltr artifacts/*png
-popd
